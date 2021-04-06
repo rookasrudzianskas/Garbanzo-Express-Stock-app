9c44a7cf507b2ef107e032c759016494198d5f98
Rokas <rookas.rudzenskas@gmail.com>
2021-04-06 15:47:05 +0300

    Rokas

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const express = require('express');
 const app = express();
 const path = require('path');
 var exphbs  = require('express-handlebars');
-
+const request = require('request');
 
 const PORT = process.env.PORT || 5000;
+
+//pk_be5627477eda4219ab8663c382811c82
+request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_be5627477eda4219ab8663c382811c82', { json: true }, (err, res, body) => {
+   if(err) {return console.log(err);}
+   if(res.statusCode === 200) {
+       console.log(body);
+   };
+});
+
+
 
 const otherStuff = "This is otehr stuff";
 
@@ -18,6 +28,12 @@ app.get('/', function (req, res) {
     res.render('home', {
         stuff: otherStuff
     });
+});
+
+//create about apge rount
+
+app.get('/about.html', function (req, res) {
+    res.render('about');
 });
 
 
