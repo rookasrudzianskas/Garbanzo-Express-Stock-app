533cf34e2e727beaab1ec5fb1cead8ea924ff943
Rokas <rookas.rudzenskas@gmail.com>
2021-04-06 14:32:30 +0300

    Rokas

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+
+
+app.listen(PORT, () => console.log("Server listening on port " + PORT));
