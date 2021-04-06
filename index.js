578772444823380557b78c3b7da576cf7c47aaa4
Rokas <rookas.rudzenskas@gmail.com>
2021-04-06 14:44:04 +0300

    Rokas

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+var exphbs  = require('express-handlebars');
+
+
 const PORT = process.env.PORT || 5000;
 
+
+
+//Set handlebars middleware
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+//Set hadnlebar routes
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-
-
-
 
 app.listen(PORT, () => console.log("Server listening on port " + PORT));
